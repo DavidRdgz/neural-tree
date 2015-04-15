bf05798e5c776f9b25243d28428cfbdbe7b9996d
davidrdgz <dvidr@mail.sfsu.edu>
2015-04-15 14:13:08 -0700

    Set as reference class. Debugged.

--- node.R
+++ node.R
@@ -1,106 +1,99 @@
-library(entropy) 
-library(nnet) 
+library(nnet)
+library(entropy)
 
-NODE <- function(...)
-{
-        data <- list(
-            id = 0,
-            parent = 0,
-            s.l = 0,
-            s.r = 0,
-            s = c(),
-            h = list(),
-            h.i = list()
-       )
+Node <- setRefClass(Class = "Node",
+                    fields = list(
+                                  id     = "numeric",
+                                  parent = "numeric",
+                                  s      = "data.frame",
+                                  s.l    = "list",
+                                  s.r    = "list",
+                                  h      = "list",
+                                  h.i    = "list"
+                                  ),
+                    methods = list(
+                                   set = function(...){
+                                        args   <- list(...)
 
-        ## Set the name for the class
-        class(data) <- append(class(data),"NODE")
-        return(data)
-}
+                                        Map(assign, names(args), args)
+                                   },
+                                   init.split = function(df, ...){
+                                       args   <- list(...)
+                                       tble <- table(df$l)
+                                       e <- entropy.empirical(tble, unit = "log2")
 
-set.node <- function(...) UseMethod("set.node")
-set.node.NODE <- function(...){
-    args   <- list(...)
-    object <- args[["object"]] 
+                                       # Check if 's' is worth splitting
+                                       if (e < .35) {
+                                           return(FALSE)
+                                       } else {
+                                           return(TRUE)
+                                       }
+                                   },
+                                   init.splits = function(...) {
+                                       args <- list(...)
 
-    within(object,{
-        for (i in names(args)) {
-            assign(i, args[[i]], inherits = TRUE)
-        }
-        return(object)
-    })
-}
+                                       # Ok 's' is worth splitting
+                                       rez <- list()
+                                       for (i in levels(s$l)) {
+                                           classj <- (s$l == i)
+                                           nnet.fit  <- nnet(classj ~ ., data = s[,!names(s) %in% c("l")], size = 1, trace=FALSE )
+                                           nnet.pred <- predict(nnet.fit, s[,!names(s) %in% c("l") ], type = "raw")
+                                           rez[[i]]  <- list("net.fit"  = nnet.fit, 
+                                                             "net.pred" = nnet.pred, 
+                                                             "classj"   = classj)
+                                       }
+                                       h.i <<- rez
+                                   },
+                                   eval.splits = function(...) { 
+                                       args  <- list(...)
 
-init.split.node <- function(...) UseMethod("init.split.node")
-init.split.node.NODE <- function(...){
-    args   <- list(...)
-    object <- args[["object"]]
+                                       L          <- h.i
+                                       labels     <- names(L)
 
-    s <- object$s 
+                                       nnet.fit   <- lapply(L,"[[","net.fit")
+                                       nnet.pred  <- lapply(L,"[[","net.pred")
+                                       candidates <- lapply(nnet.pred, function(x) round(x) == 1)
 
-    rez <- list()
-    for (i in levels(s$l)) {
-		type      <- (s$l == i)
-		nnet.fit  <- nnet(type ~ ., data = s[,!names(s) %in% c("l")], size = 1)
-		nnet.pred <- predict(nnet.fit, s[,!names(s) %in% c("l") ], type = "raw")
-        rez[[i]]  <- list("net.fit"  = nnet.fit, 
-                          "net.pred" = nnet.pred, 
-                          "classj"   = type)
-    }
-    object$h.i <- rez
-    return(object)
-}
+                                       h.s.r <- sapply(candidates, function(x) s.entropy(s, x))
+                                       h.s.l <- sapply(candidates, function(x) s.entropy(s, !x))
+                                       h.s   <- s.entropy(s, rep(TRUE, nrow(s)))
+                                       delta.h.s <- h.s + h.s.l - h.s.r
 
-s.entropy <- function(...) {
+                                       rank <- order(delta.h.s, decreasing = TRUE)
+                                       h <<- list("nets"       = nnet.fit[rank],
+                                                  "candidates" = candidates[rank],
+                                                  "delta.h.s"  = delta.h.s[rank],
+                                                  "h.s.r"      = h.s.r[rank],
+                                                  "h.s.l"      = h.s.l[rank])
+                                   },
+                                   split = function(...){
+                                       args   <- list(...)
+
+                                       candidates <- h$candidates[[1]]
+                                       s.r <<- list("candidates" = candidates,
+                                                    "s" = s[candidates, ],
+                                                    "h" = h$h.s.r[[1]])
+                                       s.l <<- list("candidates" = !candidates,
+                                                    "s" = s[!candidates, ],
+                                                    "h" = h$h.s.l[[1]])
+                                   },
+                                   initialize = function(...) {
+                                       callSuper(...)
+                                       #
+                                       # Initialise fields here (place holder)...
+                                       #
+                                       .self
+                                   }
+                                   )
+                    )
+
+s.entropy <- function(df, subset, ...) {
     args <- list(...)
-    df <- args[["df"]]
-    x  <- args[["subset"]]
+    x  <- subset
 
     p <- sum(x)
     n <- length(x)
     return(p/n *entropy.empirical(table(df[x, "l"]), unit = "log2"))
-}
-
-eval.splits <- function(...) UseMethod("eval.splits")
-eval.splits.NODE <- function(...) { 
-    args  <- list(...)
-    object <- args[["object"]]
-  
-    L          <- object$h.i
-    labels     <- names(L)
-
-    nnet.fit   <- lapply(L,"[[","net.fit")
-    nnet.pred  <- lapply(L,"[[","net.pred")
-    candidates <- lapply(nnet.pred, function(x) round(x) == 1)
-
-    h.s.r <- sapply(candidates, function(x) s.entropy(df = object$s, subset = x))
-    h.s.l <- sapply(candidates, function(x) s.entropy(df = object$s, subset = !x))
-    h.s   <- s.entropy(object = object$s, subset = rep(1, nrow(object$s))==1)
-    delta.h.s <- h.s + h.s.l - h.s.r
-
-    rank <- order(delta.h.s, decreasing = TRUE)
-    object$h <- list("nets"       = nnet.fit[rank],
-                     "candidates" = candidates[rank],
-                     "delta.h.s"  = delta.h.s[rank],
-                     "h.s.r"      = h.s.r[rank],
-                     "h.s.l"      = h.s.l[rank])
-                    
-    return(object)
-}
-
-split.node <- function(...) UseMethod("split.node")
-split.node.NODE <- function(...){
-    args   <- list(...)
-    object <- args[["object"]]
-
-    candidates <- object$h$candidates[[1]]
-    object$s.r <- list("candidates" = candidates,
-                       "s" = object$s[candidates, ],
-                       "h" = object$h$h.s.r[[1]])
-    object$s.l <- list("candidates" = !candidates,
-                       "s" = object$s[-candidates, ],
-                       "h" = object$h$h.s.l[[1]])
-    return(object)
 }
 
 sample <- function(...){
@@ -109,11 +102,13 @@ sample <- function(...){
     tmp.df <- iris
     colnames(tmp.df)[5] <- "l"
 
-    n <- NODE()
-    n <- set.node(object = n, s = tmp.df)
-    n <- init.split.node(object = n)
+    n <- Node(id = "000", s = tmp.df)
 
-    n <- eval.splits(object = n)
-    n <- split.node(object = n)
+    if (n$init.split()) {
+        n$init.splits()
+        n$eval.splits()
+        n$split()
+    }
     return(n)
 }
+
