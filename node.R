e776cb49b25669a8e90c09a3f56539e9c0344e3f
davidrdgz <dvidr@mail.sfsu.edu>
2015-04-18 15:50:52 -0700

    Add parallel and rethinking class members.

--- node.R
+++ node.R
@@ -4,11 +4,13 @@ library(parallel)
 
 Node <- setRefClass(Class = "Node",
                     fields = list(
-                                  id     = "numeric",
-                                  parent = "numeric",
-                                  s      = "data.frame",
-                                  s.l    = "list",
-                                  s.r    = "list",
+                                  id      = "numeric",
+                                  parent  = "numeric",
+                                  label   = "character",
+                                  s       = "data.frame",
+                                  #net     = "nnet",
+                                  s.l     = "list",
+                                  s.r     = "list",
                                   purity.candidates = "list",
                                   net.candidates    = "list"
                                   ),
@@ -17,32 +19,44 @@ Node <- setRefClass(Class = "Node",
                                        args   <- list(...)
                                        Map(assign, names(args), args)
                                    },
-                                   init.split = function(...){
+                                   is.leaf = function(...){
                                        args   <- list(...)
-
-                                       e <- entropy.empirical(table(s$l), unit = "log2")
+                                       tble <- sort(table(s$l), decreasing = TRUE)
+                                       e <- entropy.empirical(tble, unit = "log2")
+                                       print(e)
 
                                        # Check if 's' is worth splitting
                                        if (e < .35) {
-                                           return(FALSE)
-                                       } else {
+                                           label   <<- names(tble)[1]
+                                           #is.leaf <<- TRUE
                                            return(TRUE)
+                                       } else {
+                                           return(FALSE)
                                        }
                                    },
                                    init.splits = function(...) {
                                        args <- list(...)
 
+
                                        # Ok 's' is worth splitting
                                        lvls      <- levels(s$l)
-                                       classj    <- mclapply(lvls, function(x) s$l == x)
-                                       nnet.fit  <- mclapply(classj, function(x) nnet(x ~ ., data = s[,!names(s) %in% c("l")], size = 1, trace=FALSE ))
-                                       nnet.pred <- mclapply(nnet.fit, function(x) predict(x , s[,!names(s) %in% c("l") ], type = "raw"))
+                                       classj    <- lapply(lvls, function(x) s$l == x)
+
+                                            #find cores / open processes
+                                            #no_cores <- detectCores() - 1
+                                            #cl <- makeCluster(no_cores, type="FORK")
+                                            #nnet.fit  <- parLapply(cl, classj, function(x) nnet(x ~ ., data = s[,!names(s) %in% c("l")], size = 1, trace=FALSE))
+                                            #stopCluster(cl)
+
+                                       nnet.fit  <- lapply(classj, function(x) nnet(x ~ ., data = s[,!names(s) %in% c("l")], size = 1, trace=FALSE ))
+                                       nnet.pred <- lapply(nnet.fit, function(x) predict(x , s[,!names(s) %in% c("l") ], type = "raw"))
                                        rez       <- Map(list, lvls, classj, nnet.fit, nnet.pred)
                                        net.candidates <<- rez
                                    },
                                    eval.splits = function(...) { 
                                        args  <- list(...)
 
+                                       lvls       <- lapply(net.candidates, function(x) x[[1]])
                                        nnet.fit   <- lapply(net.candidates, function(x) x[[3]])
                                        nnet.pred  <- lapply(net.candidates, function(x) x[[4]])
                                        candidates <- lapply(nnet.pred, function(x) round(x) == 1)
@@ -52,23 +66,29 @@ Node <- setRefClass(Class = "Node",
                                        h.s   <- s.entropy(s, rep(TRUE, nrow(s)))
                                        delta.h.s <- h.s + h.s.l - h.s.r
 
+                                       print("candidate size: ")
+                                       print(lapply(candidates, length))
+                                       print("delta hs")
+                                       print(delta.h.s)
                                        rank <- order(delta.h.s, decreasing = TRUE)
-                                       rez  <- Map(list, nnet.fit, candidates, delta.h.s, h.s.r, h.s.l)
+                                       rez  <- Map(list, lvls, nnet.fit, candidates, delta.h.s, h.s.r, h.s.l)
 
                                        purity.candidates <<- lapply(rank, function(x) rez[[x]])
                                    },
                                    split = function(...){
                                        args   <- list(...)
 
-                                       candidates <- purity.candidates[[1]][[2]]
-                                       r.purity     <- purity.candidates[[1]][[4]]
-                                       l.purity     <- purity.candidates[[1]][[5]]
-                                       s.r <<- list("candidates" = candidates,
-                                                    "s" = s[candidates, ],
-                                                    "h" = r.purity)
-                                       s.l <<- list("candidates" = !candidates,
-                                                    "s" = s[!candidates, ],
-                                                    "h" = l.purity)
+                                       candidates  <- purity.candidates[[1]][[3]]
+                                       r.purity    <- purity.candidates[[1]][[5]]
+                                       l.purity    <- purity.candidates[[1]][[6]]
+                                       label      <<- purity.candidates[[1]][[1]]
+
+                                       s.r        <<- list("candidates" = candidates,
+                                                           "s" = s[candidates, ],
+                                                           "h" = r.purity)
+                                       s.l        <<- list("candidates" = !candidates,
+                                                           "s" = s[!candidates, ],
+                                                           "h" = l.purity)
                                    },
                                    initialize = function(...) {
                                        callSuper(...)
@@ -80,13 +100,15 @@ Node <- setRefClass(Class = "Node",
 s.entropy <- function(df, subset, ...) {
     args <- list(...)
     x  <- subset
+    print("here")
+
 
     p <- sum(x)
     n <- length(x)
     return(p/n *entropy.empirical(table(df[x, "l"]), unit = "log2"))
 }
 
-sample <- function(...){
+iris.sample <- function(...){
     args <- list(...)
 
     tmp.df <- iris
@@ -97,12 +119,12 @@ sample <- function(...){
     if (n$init.split()) {
         n$init.splits()
         n$eval.splits()
-        #n$split()
+        n$split()
     }
     return(n)
 }
 
-sample2 <- function(...){
+spam.sample <- function(...){
     args <- list(...)
 
     library(RCurl)
@@ -116,7 +138,7 @@ sample2 <- function(...){
 
     n <- Node(id = 1, parent = 0, s = spambase.data)
 
-    if (n$init.split()) {
+    if (n$is.leaf()) {
         n$init.splits()
         n$eval.splits()
         n$split()
