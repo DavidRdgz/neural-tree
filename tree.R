ee594f8931d2150b85d156ae076ce8c0af721491
davidrdgz <dvidr@mail.sfsu.edu>
2015-04-18 18:33:03 -0700

    Growth method if/then conditions clearer.

--- tree.R
+++ tree.R
@@ -11,15 +11,18 @@ Tree <- setRefClass(Class = "Tree",
                                        args <- list(...)
 
                                        iter <- iter(cur.val = 1)
-                                       while (length(queue$items) > 0 ){
+                                       while (queue$size() > 0) {
                                            tmp       <- queue$pop()
                                            s.tmp     <- tmp[[1]]
                                            id.tmp    <- tmp[[2]]
                                            p.id.tmp  <- tmp[[3]]
 
-                                           n <- Node(id = id.tmp, parent = p.id.tmp, s = s.tmp$s)
+                                           n <- Node(id = id.tmp, parent = p.id.tmp, s = s.tmp$s, leaf = FALSE)
 
+                                           # Is there eough impurity to try to split
                                            if (!n$is.leaf()) {
+                                               # Yes, but now check if single layer neural net
+                                               # achieves enough purity
                                                print("Splits")
                                                n$init.splits()
                                                print("Order Splits")
@@ -28,21 +31,30 @@ Tree <- setRefClass(Class = "Tree",
                                                n$split()
 
                                                tree <<- append(tree, n)
-                                               if (nrow(n$s.r$s) > 0) {
+
+                                               if (nrow(n$s.r$s) > 0 && nrow(n$s.l$s) > 0) {
                                                    queue$push(list(n$s.r, iter$inc(), id.tmp))
                                                    queue$push(list(n$s.l, iter$inc(), id.tmp))
+                                               } else {
+                                                   # Not enough impurity to split based on neural net
+                                                   # set as leaf
+                                                   n$set(leaf = TRUE)
                                                }
                                            } else {
+                                               # Not enough impurity to try initial split
+                                               n$set(leaf = TRUE)
                                                tree <<- append(tree, n)
                                            }
                                        }
-
                                    },
                                    peek = function(...) {
                                        length(tree)
                                    },
                                    get.labels = function(...) {
                                        lapply(tree, function(x) x[["label"]])
+                                   },
+                                   get.leaves = function(...) {
+                                       lapply(tree, function(x) if(x$leaf == TRUE) x$label)
                                    },
                                    initialize = function(...) {
                                        callSuper(...)
@@ -69,21 +81,26 @@ iter <- setRefClass(Class = "iter",
                     )
 
 iris.sample <- function(){
+    ########
+    #
+    # Prereqs: e < .35
+    #
+    #######
     df <- iris
     colnames(df)[5] <- "l"
 
-    q <- Queue(items = list(
-                            list(
-                                 list(s = df), 1,0)
-                            )
-    )
-
+    q <- Queue(items = list( list(list(s = df), 1,0)))
     t <- Tree(tree = list(), queue= q)
     t$grow()
     t
 }
 
 spam.sample <- function(){
+    ########
+    #
+    # Prereqs: e < .5
+    #
+    #######
     library(RCurl)
     spambase.file <- "https://archive.ics.uci.edu/ml/machine-learning-databases/spambase/spambase.data"
     spambase.url  <- getURL(spambase.file)
@@ -91,18 +108,18 @@ spam.sample <- function(){
     colnames(spambase.data)[ncol(spambase.data)] <- "l"
     spambase.data$l <- factor(spambase.data$l)
 
-    q <- Queue(items = list(
-                            list(
-                                 list(s = spambase.data),1,0)
-                            )
-    )
-
+    q <- Queue(items = list(list(list(s = spambase.data),1,0)))
     t <- Tree(tree = list(), queue= q)
     t$grow()
     t
 }
 
 letter.sample <- function(){
+    ########
+    #
+    # Prereqs: e < .5 
+    #
+    #######
     library(RCurl)
     letter.file <- "https://archive.ics.uci.edu/ml/machine-learning-databases/letter-recognition/letter-recognition.data"
     letter.url  <- getURL(letter.file)
@@ -111,14 +128,8 @@ letter.sample <- function(){
     letter.data$l <- factor(letter.data$l)
     letter.data   <-subset(letter.data, l %in% c("A","B","C","D"))
 
-    q <- Queue(items = list(
-                            list(
-                                 list(s = letter.data),1,0)
-                            )
-    )
-
+    q <- Queue(items = list(list(list(s = letter.data),1,0)))
     t <- Tree(tree = list(), queue= q)
-
     t$grow()
     t
 }
