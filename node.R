4d8c15f81916d750c9a814a643cca904b8c048e9
davidrdgz <dvidr@mail.sfsu.edu>
2015-04-29 19:03:03 -0700

    clean up

--- node.R
+++ node.R
@@ -38,14 +38,6 @@ Node <- setRefClass(Class = "Node",
                                        lvls      <- levels(S$l)
                                        classj    <- lapply(lvls, function(x) S$l == x)
                                        r.p <- sapply(classj, function(x) sum(x)/length(x))
-                                       exists.trump <- r.p >.65
-
-                                       #if (sum(exists.trump)) {
-                                       #    trump <- lvls[exists.trump]
-                                       #    classt <- S$l == trump
-                                       #    net.tmp <- nnet(classt ~ ., data = S[sample(nrow(S)),], size = 2, rang=.1, decay = 5e-4, maxit = 400, trace = FALSE)
-                                       #    right.candidate <- trump
-                                       #} else {
 
                                        my.grid <- expand.grid(.decay = c(0.5, 0.1), .size = c(1,2,3))
 
@@ -62,13 +54,6 @@ Node <- setRefClass(Class = "Node",
 
                                        r.pred <- lapply(classj, function(x) predict(net.tmp, S[x,], type = "class"))
                                        l.pred <- lapply(classj, function(x) predict(net.tmp, S[!x,], type = "class"))
-                                       #retry <- Map(function(x,y) x %in% y, lvls, names(table(r.pred)))
-                                       #print("Retry")
-                                       #print(retry)
-                                       print("S$l distribution")
-                                       print(table(S$l))
-                                       print("Classj predictions")
-                                       print(sapply(r.pred,table))
 
                                        #evaluate which label gives best entropy score
                                        e.r <- sapply(r.pred, function(x) entropy.empirical(table(x)))
@@ -80,17 +65,10 @@ Node <- setRefClass(Class = "Node",
                                        e <- entropy.empirical(table(S$l))
                                        d.e <- e - (1-r.p)*e.l +  r.p*e.r
                                        names(d.e) <- lvls
-                                       print("Entropy scores")
-                                       print(sort(d.e, decreasing = TRUE))
                                        
                                        right.candidate <- names(sort(d.e, decreasing = TRUE))[1]
-                                       print("R2 subset")
-                                       print(right.candidate)
-                                       #}
 
                                        pred <- predict(net.tmp, S, type = "class")
-                                       print("Prediction distribution")
-                                       print(table(pred))
                                        R.tmp <- droplevels(S[pred == right.candidate, ])
                                        L.tmp <- droplevels(S[pred != right.candidate, ])
                                        
@@ -107,19 +85,4 @@ Node <- setRefClass(Class = "Node",
                                    )
                     )
 
-iris.sample <- function(cut.off, ...){
-    args <- list(...)
-
-    tmp.df <- iris
-    colnames(tmp.df)[5] <- "l"
-    tmp.df$l <- factor(tmp.df$l)    
-
-    n <- Node(id = 1, parent = 0, S = tmp.df, leaf = FALSE)
-
-    n$is.pure(cut.off)
-    if (!n$leaf) {
-        n$split()
-    }
-    return(n)
-}
 
